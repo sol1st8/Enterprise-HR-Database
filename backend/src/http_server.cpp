36272d414dbc3a25cb627d9d10993672987b5c8c
Pavel <zhdanpav59rus@gmail.com>
2024-10-21 21:47:24 +0300

    feature/http_server

---
+++ backend/src/http_server.cpp
@@ -0,0 +1,100 @@
+#include "http_server.h"
+
+#include <iostream>
+#include <stdexcept>
+
+namespace http_server {
+
+void ReportError(beast::error_code ec, std::string_view what) {
+    std::cerr << what << ": "sv << ec.message() << std::endl;
+}
+
+SessionBase::SessionBase(tcp::socket&& socket)
+    : stream_(std::move(socket)), remote_endpoint_(socket.remote_endpoint()) {}
+
+void SessionBase::Run() {
+    net::dispatch(stream_.get_executor(), beast::bind_front_handler(&SessionBase::Read, GetSharedThis()));
+}
+
+void SessionBase::Read() {
+    request_ = {};
+    stream_.expires_after(30s);
+    http::async_read(stream_, buffer_, request_, beast::bind_front_handler(&SessionBase::OnRead, GetSharedThis()));
+}
+
+void SessionBase::OnRead(beast::error_code ec, [[maybe_unused]] size_t bytes_read) {
+    if (ec == http::error::end_of_stream) {
+        return Close();
+    }
+    if (ec) {
+        return ReportError(ec, "read"sv);
+    }
+    HandleRequest(std::move(request_));
+}
+
+void SessionBase::OnWrite(bool close, beast::error_code ec, [[maybe_unused]] size_t bytes_writter) {
+    if (close) {
+        return Close();
+    }
+    if (ec) {
+        return ReportError(ec, "write"sv);
+    }
+    Read();
+}
+
+void SessionBase::Close() {
+    stream_.socket(). shutdown(tcp::socket::shutdown_send);
+}
+
+} // namespace http_server
+
+namespace url_decode {
+
+int IntFromHex(char hex) {
+    if ('0' <= hex && hex <= '9') {
+        return hex - '0';
+    }
+    else if ('A' <= hex && hex <= 'F') {
+        return hex - 'A' + 10;
+    }
+    else if ('a' <= hex && hex <= 'f') {
+        return hex - 'a' + 10;
+    }
+    return -1;
+}
+
+int IntFrom2Hex(char first, char second) {
+    return IntFromHex(first) * 16 + IntFromHex(second);
+}
+
+std::string DecodeURL(std::string_view url) {
+    size_t size = url.size();
+    size_t i = 0;
+    std::string result;
+    result.reserve(url.size());
+
+    while (i < size) {
+        char curr = url.at(i);
+        if (curr == '%') {
+            if (i > size - 3) {
+                throw std::runtime_error("Incorrect url");
+            }
+            int hex = IntFrom2Hex(url.at(i + 1), url.at(i + 2));
+            if (hex < 0) {
+                throw std::runtime_error("Incorrect hex number");
+            }
+            result.push_back(static_cast<char>(hex));
+            i += 2;
+        }
+        else if (curr == '+') {
+            result.push_back(' ');
+        }
+        else {
+            result.push_back(curr);
+        }
+        ++i;
+    }
+    return result;
+}
+
+} // namespace url_decode
---
+++ backend/src/main.cpp
@@ -0,0 +1,83 @@
+#include "http_server.h"
+#include "sdk.h"
+
+#include <boost/asio/signal_set.hpp>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+namespace {
+using namespace std::literals;
+namespace net = boost::asio;
+namespace sys = boost::system;
+namespace http = boost::beast::http;
+
+using StringRequest = http::request<http::string_body>;
+using StringResponse = http::response<http::string_body>;
+
+struct ContentType {
+    ContentType() = delete;
+    constexpr static std::string_view TEXT_HTML = "text/html"sv;
+};
+
+StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version,
+                                  bool keep_alive, std::string_view content_type = ContentType::TEXT_HTML) {
+    StringResponse response(status, http_version);
+    response.set(http::field::content_type, content_type);
+    response.body() = body;
+    response.content_length(body.size());
+    response.keep_alive(keep_alive);
+    return response;
+}
+
+StringResponse HandleRequest(StringRequest&& req) {
+    const auto text_response = [&req](http::status status, std::string_view text) {
+        return MakeStringResponse(status, text, req.version(), req.keep_alive());
+    };
+    if (req.method_string() == "GET"sv) {
+        return text_response(http::status::ok, std::string("Hello, "s + std::string(req.target()).substr(1)));
+    }
+    else if (req.method_string() == "HEAD"sv) {
+        return text_response(http::status::ok, ""sv);
+    }
+    return text_response(http::status::method_not_allowed, "Invalid method"sv);
+}
+
+template <typename Fn>
+void RunWorkers(unsigned n, const Fn& fn) {
+    n = std::max(1u, n);
+    std::vector<std::jthread> workers;
+    workers.reserve(n - 1);
+    while (--n) {
+        workers.emplace_back(fn);
+    }
+    fn();
+}
+
+} // namespace
+
+int main() {
+    const unsigned num_threads = std::thread::hardware_concurrency();
+
+    net::io_context ioc(num_threads);
+
+    net::signal_set signals(ioc, SIGINT, SIGTERM);
+    signals.async_wait([&ioc](const sys::error_code& ec, [[maybe_unused]] int signal_number) {
+        if (!ec) {
+            ioc.stop();
+        }
+    });
+
+    const auto address = net::ip::make_address("0.0.0.0");
+    constexpr net::ip::port_type port = 8080;
+    http_server::ServeHttp(ioc, {address, port}, [](auto&& req, auto&& sender) {
+        sender(HandleRequest(std::forward<decltype(req)>(req)));
+    });
+
+    std::cout << "Server has started..."sv << std::endl;
+
+    RunWorkers(num_threads, [&ioc] {
+        ioc.run();
+    });
+}
